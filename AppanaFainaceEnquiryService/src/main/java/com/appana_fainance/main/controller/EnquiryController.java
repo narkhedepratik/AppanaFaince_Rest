f19eaeeca8a1d0056ae66eed7f34371732ef6d81
waykar-vaishnavi <waykarvaishnavi144@gmail.com>
2024-03-11 10:07:09 +0530

    post Api in enquiry

--- AppanaFainaceEnquiryService/src/main/java/com/appana_fainance/main/controller/EnquiryController.java
+++ AppanaFainaceEnquiryService/src/main/java/com/appana_fainance/main/controller/EnquiryController.java
@@ -1,7 +1,21 @@
 package com.appana_fainance.main.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.appana_fainance.main.model.Enquiry;
+
+@RestController
 public class EnquiryController {
 	
+	
+	
+	@PostMapping("/enquiry")
+   public Enquiry createEnquiry(@RequestBody Enquiry enquiry)
+   {
+		return enquiry;
+   }
 	
 
 }
