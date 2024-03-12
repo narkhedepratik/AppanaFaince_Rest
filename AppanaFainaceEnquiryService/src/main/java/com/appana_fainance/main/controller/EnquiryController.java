5be168671c93fd417452800ddd472822a38ba81b
narkhedepratik <pratiknarkhede039@gmail.com>
2024-03-12 10:32:45 +0530

    get single enq API

--- AppanaFainaceEnquiryService/src/main/java/com/appana_fainance/main/controller/EnquiryController.java
+++ AppanaFainaceEnquiryService/src/main/java/com/appana_fainance/main/controller/EnquiryController.java
@@ -1,5 +1,6 @@
 package com.appana_fainance.main.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +17,17 @@ public class EnquiryController {
    {
 		return enquiry;
    }
+
 	@GetMappling("/enquiry")
 	public List<Enquiry> allEnquiry()
 	{
 		return null;
+
+	}
+	@GetMapping("/enquiry/{id}")
+	public void getSingleEnquiry()
+	{
+		
 
 	}
 	
